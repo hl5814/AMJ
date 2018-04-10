181491809cc5dfa8ce06db638e10790735a3681e
Hongtao  Li <hongtao.li14@imperial.ac.uk>
2018-04-10 20:50:04 +0800

    implement script for updating all expected results for pre-defined test cases

--- testAll.sh
+++ testAll.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
+# parse command line flag "-u" for update all
+# expected results for pre-defined test cases
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -u|--update)
+    UPDATE=1
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+# loop through pre-defined test cases
 for i in 0 1 2 3 4 5 6 7 8 9 10;
 do
 	echo -ne "Program $i: \t"
+	if [ ${UPDATE} ] ;
+	then
+		echo -e "\033[33m [update] \033[0m"
+		node main.js -v -t $i > testProgramsResults/$i
+		continue
+	fi
+
 	node main.js -v -t $i > testProgramsResults/temp_result
 	diff testProgramsResults/$i testProgramsResults/temp_result > testProgramsResults/temp_diff
+
+
 	if [ $? -ne 0 ] ; 
 	then
 		echo -e "\033[31m [FAIL] \033[0m"
@@ -14,4 +43,12 @@ do
 	fi
 done
 
-rm testProgramsResults/temp_result testProgramsResults/temp_diff
+
+# clean up intermediate files if exits
+if [ -f testProgramsResults/temp_result ]; then
+    rm testProgramsResults/temp_result
+fi
+
+if [ -f testProgramsResults/temp_diff ]; then
+    rm testProgramsResults/temp_diff
+fi
--- testPrograms.js
+++ testPrograms.js
@@ -63,7 +63,11 @@ var program10 =
 	    y = 0;
 	    eval(y);
 	}`;
-	
+
 exports.programs = [program0,program1,program2,program3,
 					program4,program5,program6,program7,
 					program8,program9,program10];
+
+
+
+
