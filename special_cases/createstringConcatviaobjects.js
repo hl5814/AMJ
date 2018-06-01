e9b367961ed6347e0136817519948c95adb0457a
Hongtao  Li <hongtao.li14@imperial.ac.uk>
2018-06-01 19:47:21 +0100

    implement basic checks for function calls

---
+++ special_cases/createstringConcatviaobjects.js
@@ -0,0 +1,27 @@
+var yctohzu = {
+        qqowype: "WS",
+        ofty: 'wyxqazaqr2'
+    }['qqowype'] + {
+        all: 'ahq6',
+        co: "cr"
+    }['co'] + {
+        ocgima: '6561',
+        axf: 'polaxipy6',
+        yjko: "ip"
+    }['yjko'] + {
+        gocoh: "t.",
+        ocwo: '7805',
+        il: '8551',
+        mvecimp: 'xjifxibykjy3'
+    }['gocoh'] + {
+        dsoqcazy: 'emdefganbyrv0',
+        tni: "Sh"
+    }['tni'] + {
+        nrynkyxc: '325',
+        pakfyb: 'ichexcimel3',
+        ynpa: "el",
+        det: '6194'
+    }['ynpa'] + {
+        kmaxnyx: '5029',
+        itma: "l"
+    }['itma'];
